2ab017da9184e57bddb8482b894e9ec85c7fd64b
= <=>
2023-10-15 08:16:21 +0000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{ car }}</div>
+  <abcd msg="My First App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import abcd from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    abcd
+  },
+  data(){
+    return {
+      car: "Benz"
+    }
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'abcdM',
   props: {
     msg: String
   }
